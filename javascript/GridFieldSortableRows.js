2285cfa7cbc90f66105b809029e7c616f4c3931d
UndefinedOffset <edchipman@gmail.com>
2012-05-15 12:46:03 -0300

    Removed reload of table after dropping item

--- javascript/GridFieldSortableRows.js
+++ javascript/GridFieldSortableRows.js
@@ -14,35 +14,54 @@
 			gridField.find('tbody').sortable({
 											disabled: ($(this).is(':checked')==false),
 											helper: function(e, ui) {
-														//Maintains width of the columns
-														ui.children().each(function() {
-															$(this).width($(this).width());
-														});
-														
-														return ui;
-													},
+												//Maintains width of the columns
+												ui.children().each(function() {
+													$(this).width($(this).width());
+												});
+												
+												return ui;
+											},
 											update: function(event, ui) {
-														var dataRows=[];
-														var gridItems=gridField.getItems();
-														var button=refCheckbox.parent().find('.sortablerows-toggle');
-														
-														
-														for(var i=0;i<gridItems.length;i++) {
-															dataRows[i]=$(gridItems[i]).data('id');
-														}
-														
-														
-														gridField.reload({data: [
-																					{
-																						name: button.attr('name'),
-																						value: button.val()},
-																					{
-																						name: 'Items',
-																						value: dataRows
-																					}
-																				]
-																			});
+												var dataRows=[];
+												var gridItems=gridField.getItems();
+												var button=refCheckbox.parent().find('.sortablerows-toggle');
+												
+												
+												for(var i=0;i<gridItems.length;i++) {
+													dataRows[i]=$(gridItems[i]).data('id');
+												}
+												
+												
+												var form = gridField.closest('form'), 
+													focusedElName = gridField.find(':input:focus').attr('name'); // Save focused element for restoring after refresh
+												var ajaxOpts = {data: [
+																		{
+																			name: button.attr('name'),
+																			value: button.val()},
+																		{
+																			name: 'Items',
+																			value: dataRows
+																		}
+																	]};
+												
+												ajaxOpts.data = ajaxOpts.data.concat(form.find(':input').serializeArray());
+												
+												// Include any GET parameters from the current URL, as the view state might depend on it.
+												// For example, a list prefiltered through external search criteria might be passed to GridField.
+												if(window.location.search) {
+													ajaxOpts.data = window.location.search.replace(/^\?/, '') + '&' + $.param(ajaxOpts.data);
+												}
+												
+												$.ajax($.extend({}, {
+													headers: {"X-Pjax" : 'CurrentField'},
+													type: "POST",
+													url: gridField.data('url'),
+													dataType: 'html',
+													error: function(e) {
+														alert(ss.i18n._t('GRIDFIELD.ERRORINTRANSACTION'));
 													}
+												}, ajaxOpts));
+											}
 										}).disableSelection();
 		},
 		

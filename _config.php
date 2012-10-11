5b0a312a9bc46aacfc5cf8a90872add4ef9d6e21
UndefinedOffset <edchipman@gmail.com>
2012-10-11 15:07:16 -0300

    Added notice about the directory name not being correct

--- _config.php
+++ _config.php
@@ -1,3 +1,6 @@
 <?php
-
+$dir=basename(dirname(__FILE__));
+if($dir!='SortableGridField') {
+    user_error('SortableGridField: Directory name must be "SortableGridField" (currently "'.$dir.'")',E_USER_ERROR);
+}
 ?>

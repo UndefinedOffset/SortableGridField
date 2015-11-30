afffa5b09ea264186b123685fc25bc02194a5cd3
UndefinedOffset <echipman@webbuildersgroup.com>
2015-11-30 15:53:09 -0400

    Fixed regression introduced in fae85f04c134d5acfa4831aaf2b10bc1d4e40157 where disableSelection() was no longer optional
    Disable selection is now forced when the "allow drag and drop" is checked

--- javascript/GridFieldSortableRows.js
+++ javascript/GridFieldSortableRows.js
@@ -67,7 +67,11 @@
 																				form.removeClass('loading');
 																			});
 												}
-											}).disableSelection();
+											});
+				
+				if(refCheckbox.hasClass('gridfield-sortablerows-noselection') || $(this).is(':checked')) {
+					gridField.find('tbody').disableSelection();
+				}
 			},
 			
 			onchange: function(e) {

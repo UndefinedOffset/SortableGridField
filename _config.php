e6c2bc712c7221dad2ac92be5e08389f094b14e9
Ralph Slooten <ralph@technojoy.co.nz>
2018-08-08 00:37:18 +1200

    Ensure compatibility with SS 3.7 & PHP 7.2 (#123)

--- _config.php
+++ _config.php
@@ -1,3 +1,8 @@
 <?php
 define('SORTABLE_GRIDFIELD_BASE', basename(dirname(__FILE__)));
-?>
+
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) {
+    class_alias('Object', 'SS_Object');
+}
